67a3c0fb0192eb4327d866fc4539869f86d8c437
Alex Martin <alex.martin@bullish.com>
2023-12-18 21:15:58 -0500

    Include working example of persistence w/o DB Rider

--- src/test/java/alex/demo/entities/DemoEntityTest.java
+++ src/test/java/alex/demo/entities/DemoEntityTest.java
@@ -2,24 +2,34 @@ package alex.demo.entities;
 
 import alex.demo.repositories.DemoEntityRepository;
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.junit5.api.DBRider;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-@DBRider
-@MicronautTest
+@MicronautTest(transactional = false)
 public class DemoEntityTest {
 
     @Inject
     DemoEntityRepository demoEntityRepository;
 
+    @DBRider
     @DataSet(value = "dataset/seed.json")
     @Test
     void can_load_entity() {
         Long count = demoEntityRepository.count();
         Assertions.assertEquals(1, count);
+    }
+
+    @Test
+    void can_assert_saved() {
+        DemoEntity entity = DemoEntity.builder()
+                .name("test")
+                .build();
+        demoEntityRepository.save(entity);
+        Assertions.assertEquals(1, demoEntityRepository.count());
     }
 
 }
