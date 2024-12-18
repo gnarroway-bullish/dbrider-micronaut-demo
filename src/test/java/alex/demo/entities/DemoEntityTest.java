5caac9de49c0643fc18cbe2e4c23a449abeb9148
George Narroway <george.narroway@bullish.com>
2024-12-18 13:53:03 +0800

    Fix it for 3.6 by unwrapping the datasource
    - presumably something the way micronaut handles this wrapped datasource changed between 3.5 and 3.6
    - see DelegatingDataSource.unwrapDataSource(dataSource) in the ConnectionManager
    - note it now works for both @MicronautTest(transaction=false or true)

--- src/test/java/alex/demo/entities/DemoEntityTest.java
+++ src/test/java/alex/demo/entities/DemoEntityTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
-@MicronautTest()
+@MicronautTest(transactional = false)
 @DBUnit(cacheConnection = false,
         caseInsensitiveStrategy = Orthography.LOWERCASE,
         schema = "public")
